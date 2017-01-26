d4f64b4a5ab8f5c7d74fa618105ed25b908cc062
Derry Nixon <nixondc93@gmail.com>
2017-01-25 20:53:24 -0800

    added photos

--- app/assets/javascripts/react/components/_searchResults.js.jsx
+++ app/assets/javascripts/react/components/_searchResults.js.jsx
@@ -2,35 +2,103 @@ class SearchResults extends React.Component{
 
   constructor(props){
     super(props);
+    this.state = {
+      results: this.props.results
+    }
 
   }
 
-  speciesImages
+  xmlToJson(xml){
+
+  	// Create the return object
+  	var obj = {};
+
+  	if (xml.nodeType == 1) { // element
+  		// do attributes
+  		if (xml.attributes.length > 0) {
+  		obj["@attributes"] = {};
+  			for (var j = 0; j < xml.attributes.length; j++) {
+  				var attribute = xml.attributes.item(j);
+  				obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
+  			}
+  		}
+  	} else if (xml.nodeType == 3) { // text
+  		obj = xml.nodeValue;
+  	}
+
+  	// do children
+  	if (xml.hasChildNodes()) {
+  		for(var i = 0; i < xml.childNodes.length; i++) {
+  			var item = xml.childNodes.item(i);
+  			var nodeName = item.nodeName;
+  			if (typeof(obj[nodeName]) == "undefined") {
+  				obj[nodeName] = this.xmlToJson(item);
+  			} else {
+  				if (typeof(obj[nodeName].push) == "undefined") {
+  					var old = obj[nodeName];
+  					obj[nodeName] = [];
+  					obj[nodeName].push(old);
+  				}
+  				obj[nodeName].push(this.xmlToJson(item));
+  			}
+  		}
+  	}
+  	return obj;
+  }
+
+  componentWillMount(){
+    this.state.results.map((result, idx) => {
+      let nameArr = result.scientific_name.split(' ')
+      result.image = undefined;
+      $.ajax({
+        url: `https://services.natureserve.org/idd/rest/ns/v1/globalSpecies/images?scientificName=${nameArr[0]}%20${nameArr[1]}`,
+        dataType: "xml",
+        success: (response) => {
+          result.image = this.xmlToJson(response).images.image[0]["dc:identifier"]["#text"]
+          this.setState({results: this.state.results})
+        },
+        error: (err) => {
+          console.log("fetching images failed");
+        }
+      });
+    });
+    ;
+
+  }
 
   render(){
     const Card = MaterialUi.Card;
     const CardTitle = MaterialUi.CardTitle;
     const CardText = MaterialUi.CardText;
-    let result = this.props.results.map((result) => {
-      let expandable = result.taxonomicnotes !== null
+    const CardMedia = MaterialUi.CardMedia;
+
+    let result = this.state.results.map((result, idx) => {
+      let expandable = result.taxonomicnotes !== null || result.habitat !== null || result.population !== null
 
       return (
         <div key={result.id}>
           <Card style={{margin: "10px 0 10px 18%", backgroundColor: "rgba(255, 255, 255, 0.85)"}}>
+
             <CardTitle
               showExpandableButton={expandable}
               actAsExpander={expandable}
               title={result.taxonname}
-              subtitle={`Scientific Name: ${result.scientific_name}`}
-            />
+              subtitle={`Endangered Status: ${result.assesments[0].split('\"')[5]}`}
+            >
+            {result.image !== undefined &&
+            <CardMedia expandable={true} style={{float: "right"}}>
+              <image src={result.image} style={{height: "200px", width: "200px", marginRight: "20px"}}/>
+            </CardMedia>}
+            </CardTitle>
             <CardText>
               <span
                 dangerouslySetInnerHTML=
                 {{__html:  result.taxonomicnotes == null ?
-                  "No information yet" : `<h3><b>Description:</b></h3>  ${result.taxonomicnotes}`}}
+                  `<b>Scientific Name:</b> ${result.scientific_name} <br><br> No description yet`: `<b>Scientific Name:</b> ${result.scientific_name} <br><br> <h3><b>Description:</b></h3>  ${result.taxonomicnotes}`}}
               >
               </span>
             </CardText>
+
             <CardText expandable={true}>
               <span dangerouslySetInnerHTML={{__html:
                   result.geographicrange == null ?  "" : `<h3><b>Geographic Range: </b></h3><br/> ${result.geographicrange}`}}
@@ -46,12 +114,6 @@ class SearchResults extends React.Component{
             <CardText expandable={true}>
               <span dangerouslySetInnerHTML={{__html:
                   result.population == null ?  "" : `<h3><b>Population:</b></h3> <b>Trend: ${result.populationtrend}</b><br/><br/>  ${result.population}`}}
-              >
-              </span>
-            </CardText>
-            <CardText expandable={true}>
-              <span dangerouslySetInnerHTML={{__html:
-                  result.assesments == null ?  "" : `${result.assesments}`}}
               >
               </span>
             </CardText>
--- app/assets/javascripts/react/react-libraries.js
+++ app/assets/javascripts/react/react-libraries.js
@@ -7,3 +7,5 @@ injectTapEventPlugin = require('react-tap-event-plugin'); injectTapEventPlugin()
 //Material Design Library Custom Theme
 MyRawTheme = require('./theme');
 ThemeManager = require('material-ui/lib/styles/theme-manager');
+
+XmlParser = require('xml2js').parseString;
